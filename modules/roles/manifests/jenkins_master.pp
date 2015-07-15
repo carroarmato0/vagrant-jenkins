23e26922a7011009d7278da855142751d252f522
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2015-07-15 08:40:23 +0200

    Re-add execs to initially load the Jenkisn-Job pipeline
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/roles/manifests/jenkins_master.pp
+++ modules/roles/manifests/jenkins_master.pp
@@ -1,15 +1,22 @@
 class roles::jenkins_master {
 
-   include jenkins
-   include jenkins::master
-   include common
+  include jenkins
+  include jenkins::master
+  include common
 
-   exec { 'Create DSL Pipeline':
-     command => 'sleep 30; jenkins-jobs update /vagrant/jenkins-dsl-playground/jenkins-dsl-pipeline.yaml',
-     path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-     unless  => 'test -d /var/lib/jenkins/jobs/jenkins-dsl-checkout/',
-     require => [ Class['jjb'], Service['jenkins'], ],
-   }
+  exec { 'Create DSL Pipeline':
+    command => 'sleep 30; jenkins-jobs update /vagrant/jenkins-dsl-playground/jenkins-dsl-pipeline.yaml',
+    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    unless  => 'test -d /var/lib/jenkins/jobs/jenkins-dsl-checkout/',
+    require => [ Class['jjb'], Service['jenkins'], ],
+  }
 
+  exec { 'Initial DSL Pipeline Trigger':
+    command => 'curl --silent --show-error http://localhost:8080/job/jenkins-dsl-checkout/build && touch /home/vagrant/.initialbuildsuccess',
+    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    unless  => 'test -d /var/lib/jenkins/jobs/jenkins-dsl-checkout/',
+    creates => '/home/vagrant/.initialbuildsuccess',
+    require => [ Class['jjb'], Service['jenkins'], ],
+  }
 
 }
