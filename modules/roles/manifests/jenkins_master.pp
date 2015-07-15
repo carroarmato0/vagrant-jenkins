75d6d06fd41c397111e0120e05539643456af9c4
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2015-07-15 08:14:37 +0200

    Re-add exec to load initial jenkins-dsl pipeline to pull the rest in
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- modules/roles/manifests/jenkins_master.pp
+++ modules/roles/manifests/jenkins_master.pp
@@ -4,4 +4,12 @@ class roles::jenkins_master {
    include jenkins::master
    include common
 
+   exec { 'Create DSL Pipeline':
+     command => 'sleep 30; jenkins-jobs update /vagrant/jenkins-dsl-playground/jenkins-dsl-pipeline.yaml',
+     path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+     unless  => 'test -d /var/lib/jenkins/jobs/jenkins-dsl-checkout/',
+     require => [ Class['jjb'], Service['jenkins'], ],
+   }
+
+
 }
