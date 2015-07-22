996f4244e0ea78e5c0a2437dcecac28254771dbf
Christophe Vanlancker <christophe.vanlancker@inuits.eu>
2015-07-22 10:59:54 +0200

    Add rpmforge-extra repo and force git to latest to overcome Git plugin issue in Jenkins regarding unsupported option
    Signed-off-by: Christophe Vanlancker <christophe.vanlancker@inuits.eu>

--- manifests/site.pp
+++ manifests/site.pp
@@ -23,7 +23,18 @@ class common {
 
   include jjb
   include timezone
-  package { 'git': }
+  package { 'git':
+    ensure  => latest,
+    require => Yumrepo['rpmforge-extras'],
+  }
+
+  yumrepo { 'rpmforge-extras':
+    baseurl    => 'http://apt.sw.be/redhat/el6/en/$basearch/extras',
+    descr      => 'RHEL $releasever - RPMforge.net - extras',
+    enabled    => '1',
+    gpgcheck   => '0',
+    mirrorlist => 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge-extras',
+  }
 
 }
 
